d37835f43124d8c35e96a94e4b2d78bab0443672
jakegodsall <jake.edward.godsall@gmail.com>
2022-09-04 10:18:55 +0100

    dynamically generate UserList from 'users' array in App.js

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -3,11 +3,19 @@ import React from 'react';
 import Card from '../UI/Card/Card';
 import User from '../User/User';
 
-const UserList = () => {
+const UserList = props => {
+    console.log(props.users);
     return (
-        <Card>        
-            <User name="Max" age="31" />
-            <User name="Max" age="31" />
+        <Card>
+            {props.users.map(user => {
+                return (
+                    <User 
+                    name={user.name}
+                    age={user.age}
+                    key={user.id}
+                />
+                );
+            })}
         </Card>
         
     );
