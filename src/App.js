7b9ee8dba42d54f9684ad139ecc46530558a6c48
jakegodsall <jake.edward.godsall@gmail.com>
2022-09-04 10:17:49 +0100

    created dummy list of 'user' objects

--- src/App.js
+++ src/App.js
@@ -5,12 +5,29 @@ import UserList from "./components/UserList/UserList";
 
 import './App.css';
 
+const users = [
+  {
+    name: 'Jake',
+    age: 27,
+    id: 1
+  },
+  {
+    name: 'Patrycjo',
+    age: 27,
+    id: 2
+  },
+  {
+    name: 'Nunu',
+    age: 4,
+    id: 3
+  }
+];
 
 const App = () => {
   return (
     <>
       <UserForm />
-      <UserList />
+      <UserList users={users}/>
     </>
     
   );
